d951c13acad54c0ca5589e479b188e4a9a2d785b
Adelina Gorodetskaya <delyunchik@gmail.com>
2024-02-28 15:16:03 +0300

    task 1

---
+++ 1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_excel('task 14.xls')
+
+loc = df.loc[(df['Фамилия'] == 'Колесникова') & (df['Имя'] == 'Ксения')]
+print(loc)
+for row in loc.values:
+    print(f'Ты получил: {row[4]}, за предмет {row[3]}')
+
+avg = round(df['Оценка за любимый предмет'].mean(), 3)
+print(f'Средний балл по классу: {avg}')
