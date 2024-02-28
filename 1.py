ccd4ff5e9a7779264ed2b36a2741e2e962c8cabb
Adelina Gorodetskaya <delyunchik@gmail.com>
2024-02-28 15:27:52 +0300

    task 1

--- 1.py
+++ 1.py
@@ -2,10 +2,15 @@ import pandas as pd
 
 df = pd.read_excel('task 14.xls')
 
-loc = df.loc[(df['Фамилия'] == 'Колесникова') & (df['Имя'] == 'Ксения')]
+loc = df[(df['Фамилия'] == 'Колесникова') & (df['Имя'] == 'Ксения')]
 
 for row in loc.values:
     print(f'Ты получил: {row[4]}, за предмет {row[3]}')
 
 avg = round(df['Оценка за любимый предмет'].mean(), 3)
 print(f'Средний балл по классу: {avg}')
+
+for subject in pd.unique(df['Любимый предмет']):
+    loc = df[df['Любимый предмет'] == subject]
+    avg = round(loc['Оценка за любимый предмет'].mean(), 3)
+    print(f'Средний балл по предмету {subject}: {avg}')
